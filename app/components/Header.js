b617bd632d23515ea097c1002d28db6bf39fc1e6
Keith B. Smith <32456218+afewvowels@users.noreply.github.com>
2025-01-27 11:25:13 -0500

    add link
    add return to homepage link

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,6 +2,9 @@ const Header = () => {
   return (
     <header>
       <h1>Inventory</h1>
+      <ul>
+        <li><a href="https://homeinventory.earth">Home</a></li>
+      </ul>
     </header>
   )
 }
